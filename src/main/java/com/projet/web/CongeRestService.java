ebc434d7a2fc1cd279dff6e0fb17e4fb3104d630
souhaayadi <ayadisouhaaa@gmail.com>
2020-07-13 20:53:05 +0100

    first commit HR

--- src/main/java/com/projet/web/CongeRestService.java
+++ src/main/java/com/projet/web/CongeRestService.java
@@ -29,74 +29,7 @@ public class CongeRestService {
 
 
 
-	/*@GetMapping("/comptes")
-	public List<Compte> getAllCompte(){
-		return compteRepository.findAll();
-	}
 
-
-	@GetMapping("/comptes/{codeCompte}")
-	public Compte consulterCompte(@PathVariable String codeCompte) {
-		Compte cp=compteRepository.findById(codeCompte).orElse(null);
-		if(cp==null) throw new RuntimeException("Compte introuvable");
-		return cp;
-	}
-
-	@PutMapping("/comptes/verser/{codeCompte}&{montant}")
-	public void verser(@PathVariable  String codeCompte, @PathVariable double montant) {
-		Compte cp=consulterCompte(codeCompte);
-		cp.setSolde(cp.getSolde()+montant);
-		Versement v =new Versement(new Date(), montant, cp);
-		operationRepository.save(v);
-	}
-
-	@PutMapping("/comptes/retirer/{codeCompte}&{montant}")
-	public void retirer(@PathVariable  String codeCompte, @PathVariable double montant) {
-		Compte cp=consulterCompte(codeCompte);
-		double faciliteCaisse=0;
-		if(cp instanceof CompteCourant)
-			faciliteCaisse=((CompteCourant) cp).getDecouvert();
-		if(cp.getSolde()+faciliteCaisse < montant)throw new RuntimeException("Solde insuffisant");
-		else {
-			cp.setSolde(cp.getSolde()-montant);
-			Retrait v =new Retrait(new Date(), montant, cp);
-			operationRepository.save(v);
-		}
-	}
-
-	@PutMapping("/comptes/virement/{codeCompte1}&{codeCompte2}&{montant}")
-	public void virement(@PathVariable String codeCompte1, @PathVariable String codeCompte2, @PathVariable double montant)
-	{
-		retirer(codeCompte1, montant);
-		verser(codeCompte2, montant);
-	}
-
-	@GetMapping("/comptes/listOperation")
-	public Page<Operation> listOperation(@RequestParam(name = "codeCompte") String codeCompte,
-										 @RequestParam (name = "page", defaultValue ="0") int page,
-										 @RequestParam (name = "size", defaultValue = "5") int size) {
-
-		return operationRepository.listOperation(codeCompte, PageRequest.of(page, size));
-
-	}
-
-	@PutMapping("/comptes/addCpCourant/{codeClient}&{codeCompte}&{solde}&{decouvert}")
-	public void addComptecourant(@PathVariable Long codeClient,@PathVariable String codeCompte ,@PathVariable double solde,@PathVariable double decouvert) {
-		Employee c= employeeRepository.findById(codeClient).orElse(null);
-		//if(typeCompte == "CC")	
-		CompteCourant cc =  new CompteCourant(codeCompte,new Date(),solde,c,decouvert);
-		compteRepository.save(cc);
-
-	}
-
-	@PutMapping("/comptes/addCpEpargne/{codeClient}&{codeCompte}&{solde}&{taux}")
-	public void addCompteEpargne(@PathVariable Long codeClient,@PathVariable String codeCompte ,@PathVariable double solde,@PathVariable double taux) {
-		Employee c= employeeRepository.findById(codeClient).orElse(null);
-		//if(typeCompte == "CC")	
-		CompteEpargne ce=new CompteEpargne(codeCompte,new Date(),solde,c,taux);
-		compteRepository.save(ce);
-	}
-*/
 
 
 }
